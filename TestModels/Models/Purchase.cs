fee4cdb2f3022421fdd2329d54a27394d5c96676
dana <bohdana.shvets@verint.com>
2016-11-24 21:03:23 +0200

    Initial commit

---
+++ TestModels/Models/Purchase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestModels.Models {
+	public class Purchase {
+		// ID покупки
+		public int PurchaseId { get; set; }
+		// имя и фамилия покупателя
+		public string Person { get; set; }
+		// адрес покупателя
+		public string Address { get; set; }
+		// ID книги
+		public int BookId { get; set; }
+		// дата покупки
+		public DateTime Date { get; set; }
+	}
+}
